9e0312a1d37d4b981ec28fd7925777fdee8708e3
soldierloko <brunomesquitaarruda@gmail.com>
2020-04-25 20:31:56 -0300

    25_04_2020

--- Python/Conn_SQL.py
+++ Python/Conn_SQL.py
@@ -1,14 +1,22 @@
+
 import pyodbc 
 
+#String de Conexão
 conn =  pyodbc.connect(
     "Driver={SQL Server Native Client 11.0};"
     "Server=DESKTOP-QRGC4CU\SQLEXPRESS;"
     "Database=AdventureWorksDW2017;"
     "Trusted_Connection=yes"
 )
+
+#Cria um Cursor Com a conexão
 cursor = conn.cursor()
+
+#Executa uma Query
 cursor.execute('SELECT TOP (1000) [ProductKey],[ProductAlternateKey] FROM [AdventureWorksDW2017].[dbo].[DimProduct]')
 
+
+#Percorre a Consulta (Mais valer colocar em um DataFranme)
 for row in cursor:
     print(f'row = {row}')
 
